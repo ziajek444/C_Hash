236385eae70a8575ff269f4db9a95a74aa5de50d
Marcin Ziajkwski <ziajkowski.marcin@gmail.com>
2018-07-09 17:07:59 +0200

    Real query
    Added some simple query and connection way

---
+++ ConsoleApp1CORE/ConsoleApp1CORE/ConsoleApp1CORE/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ConsoleApp1CORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "sql.ziajek444ci.nazwa.pl";
+                builder.UserID = "ziajek444ci_moja-db";
+                builder.Password = "SuperiorSH93";
+                builder.InitialCatalog = "ziajek444ci_moja-db";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    connection.Open();
+                    Console.WriteLine("Connected");
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("SELECT * FROM Tabela_czlonkow ");
+                    //sb.Append("FROM [SalesLT].[ProductCategory] pc ");
+                    //sb.Append("JOIN [SalesLT].[Product] p ");
+                    //sb.Append("ON pc.productcategoryid = p.productcategoryid;");
+                    String sql = sb.ToString();
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
